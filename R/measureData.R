a0e28358cc8a02c2298c6b8a188beb3404a45915
Emelia Beldon <emeliabeldon13@gmail.com>
2021-02-19 14:32:18 -0500

    new committ

--- R/measureData.R
+++ R/measureData.R
@@ -1,4 +1,33 @@
 measureData <- function(x) {
 
+specify_decimal <- function(x, k) trimws(format(round(x, k), nsmall=k))
 
+testTable <- dataTable1
+
+testTable <- mutate(testTable, American_Indian_Or_Alaska_Native_Total = as.numeric(American_Indian_Or_Alaska_Native_Total/Total_Number_of_Students))
+testTable <- mutate(testTable, Asian_Total = as.numeric(Asian_Total/Total_Number_of_Students))
+testTable <- mutate(testTable, Black_Or_African_American_Total = as.numeric(Black_Or_African_American_Total/Total_Number_of_Students))
+testTable <- mutate(testTable, Hispanic_Total = as.numeric(Hispanic_Total/Total_Number_of_Students))
+testTable <- mutate(testTable, Native_Hawaiian_Or_Other_Pacific_Islander = as.numeric(Native_Hawaiian_Or_Other_Pacific_Islander/Total_Number_of_Students))
+testTable <- mutate(testTable, White_Total = as.numeric(White_Total/Total_Number_of_Students))
+testTable <- mutate(testTable, Two_Or_More_Races_Total = as.numeric(Two_Or_More_Races_Total/Total_Number_of_Students))
+
+sampleTable <- select(testTable, American_Indian_Or_Alaska_Native_Total, Asian_Total, Black_Or_African_American_Total,
+                Hispanic_Total,Native_Hawaiian_Or_Other_Pacific_Islander,White_Total,Two_Or_More_Races_Total)
+
+dataTable1$GV_Index <<- apply(sampleTable, 1, function(t) specify_decimal((1-sum(t^2)), 3))
+
+rankedTable <<- select(dataTable1, ID,`Institution Name`, GV_Index)
+rankedTable <<- arrange(rankedTable, desc(GV_Index)) %>% mutate(Rank_of_GV_Index = 1:nrow(rankedTable))
+
+print("The table to show you the rank of the Insitutions is: rankedTable")
+
+View(rankedTable)
 }
+
+#dataTest <- dataTable1
+
+#for (i in 4:ncol(dataTest)) {
+  #print(colnames(dataTest[i]))
+  #dataTest <- mutate(dataTest, colnames(dataTest[i]) = (colnames(dataTest[i])/Total_Number_of_Students))
+#}
--- R/polishData.R
+++ R/polishData.R
@@ -1,15 +1,27 @@
 polishData <- function(x) {
+
   dataTable1 <<- select(x, -contains("men"), -UnitID, -contains("unknown"), -contains("alien"))
 
-  dataTable1 <<- rename(dataTable1, 'Total Number of Students' = 'Grand total (EF2018A  All students total)', 'American Indian Or Alaska Native Total' = 'American Indian or Alaska Native total (EF2018A  All students total)',
-                        'Asian Total' = 'Asian total (EF2018A  All students total)', 'Black Or African American Total' = 'Black or African American total (EF2018A  All students total)',
-                        'Hispanic Total' = 'Hispanic total (EF2018A  All students total)', 'Native Hawaiian Or Other Pacific Islander'
-                        = 'Native Hawaiian or Other Pacific Islander total (EF2018A  All students total)', 'White Total' = 'White total (EF2018A  All students total)',
-                        'Two Or More Races Total' = 'Two or more races total (EF2018A  All students total)')
+  dataTable1 <<- rename(dataTable1, 'Total_Number_of_Students' = 'Grand total (EF2018A  All students total)', 'American_Indian_Or_Alaska_Native_Total' = 'American Indian or Alaska Native total (EF2018A  All students total)',
+                        'Asian_Total' = 'Asian total (EF2018A  All students total)', 'Black_Or_African_American_Total' = 'Black or African American total (EF2018A  All students total)',
+                        'Hispanic_Total' = 'Hispanic total (EF2018A  All students total)', 'Native_Hawaiian_Or_Other_Pacific_Islander'
+                        = 'Native Hawaiian or Other Pacific Islander total (EF2018A  All students total)', 'White_Total' = 'White total (EF2018A  All students total)',
+                        'Two_Or_More_Races_Total' = 'Two or more races total (EF2018A  All students total)')
 
   dataTable1 <<- mutate(dataTable1, "ID" = row_number())
 
   dataTable1 <<-  dataTable1[,c(10,1,2,3,4,5,6,7,8,9)]
+
+  dataTable1 <<- mutate(dataTable1, Total_Number_of_Students = rowSums(dataTable1[,4:10]))
+
+  nameTable <- dataTable1[(dataTable1$Total_Number_of_Students==0),] ['Institution Name']
+  institutionName <- toString(nameTable$`Institution Name`[1])
+  print("These institution(s):")
+  print(institutionName)
+  print("will be removed from the data set due insufficent data.")
+
+  #removes rows with total number = 0
+  dataTable1 <<- dataTable1[!(dataTable1$Total_Number_of_Students == 0), ]
 
   print("Your new table is now called: dataTable1")
 
